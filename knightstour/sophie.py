85b4a667cc3ef3bb7c879746feb7fe7c44f019f9
sweenig <sweenig@users.noreply.github.com>
2017-01-06 13:02:56 -0600

    Added comments everywhere

--- knightstour/sophie.py
+++ knightstour/sophie.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """
 This code generates the path required for a knight's tour 
 around a chessboard with user-specified dimensions
@@ -24,109 +26,73 @@ blankboard = [[w,b,w,b,w,b,w,b],
 sense.set_pixels(sum(blankboard,[]))
 
 class KnightsTour:
+
 	def __init__(self, width, height):
-		self.w = width
-		self.h = height
-		self.board = []
-		self.generate_board()
-	def generate_board(self):
-		"""
-		Creates a nested list to represent the game board
-		"""
-		for i in range(self.h):
-			self.board.append([0]*self.w)
-	def print_board(self):
-		print "  "
-		print "------"
-		for elem in self.board:
-			print elem
-		print "------"
-		print "  "
-	def generate_legal_moves(self, cur_pos):
-		"""
-		Generates a list of legal moves for the knight to take next
-		"""
-		possible_pos = []
-		move_offsets = [(1, 2), (1, -2), (-1, 2), (-1, -2),
-						(2, 1), (2, -1), (-2, 1), (-2, -1)]
+		self.w = width #store the width in the object
+		self.h = height #store the height in the object
+		self.board = [] #make a 0 dimension board
+		for i in range(self.h): #for each column...
+			self.board.append([0]*self.w) #add a row of 0's
 
-		for move in move_offsets:
-			new_x = cur_pos[0] + move[0]
-			new_y = cur_pos[1] + move[1]
+	def generate_legal_moves(self, cur_pos): #Generates a list of legal moves for the knight to take next
+		possible_pos = [] #assume no moves are legal
+		move_offsets = [(1, 2), (1, -2), (-1, 2), (-1, -2),(2, 1), (2, -1), (-2, 1), (-2, -1)] #relative positions of possible moves
+		for move in move_offsets: #for each possible move
+			new_x = cur_pos[0] + move[0] #set new position
+			new_y = cur_pos[1] + move[1] #set new position
+			if (new_x >= self.h or new_x < 0 or new_y >= self.w or new_y < 0): #if the new position is off the board
+				continue #don't do anything (this should be turned into a single if statement, instead of an if..else
+			else: #if the new position is on the board, append it
+				possible_pos.append((new_x, new_y)) #append the validated position to the possible_position list
+		return possible_pos #return the possible position list
 
-			if (new_x >= self.h):
-				continue
-			elif (new_x < 0):
-				continue
-			elif (new_y >= self.w):
-				continue
-			elif (new_y < 0):
-				continue
-			else:
-				possible_pos.append((new_x, new_y))
-
-		return possible_pos
 	def sort_lonely_neighbors(self, to_visit):
 		"""
 		It is more efficient to visit the lonely neighbors first, 
 		since these are at the edges of the chessboard and cannot 
 		be reached easily if done later in the traversal
 		"""
-		neighbor_list = self.generate_legal_moves(to_visit)
-		empty_neighbours = []
+		neighbor_list = self.generate_legal_moves(to_visit) #get all the legal moves
+		empty_neighbours = [] #make a place to list all the empty neighbors
+		for neighbor in neighbor_list: #for each legal move
+			np_value = self.board[neighbor[0]][neighbor[1]] #get the value of the first legal move
+			if np_value == 0: #if it's zero, it hasn't been visited, so it's an empty neighbor
+				empty_neighbours.append(neighbor) #if it hasn't been visited, add it to the empty neighbors list
+		scores = [] #make a place to store the scores
+		for empty in empty_neighbours: #for all legal moves that haven't been visited
+			score = [empty, 0] #give the move a starting score of 0
+			moves = self.generate_legal_moves(empty) #figure out which of the empty & legal moves are legal (shouldn't this already be the case?)
+			for m in moves: #for all legal moves, which are empty, which are legal
+				if self.board[m[0]][m[1]] == 0: #if the move is 0, it's empty, give it a higher score
+					score[1] += 1 #increment his score
+			scores.append(score) #put the score for this move into the list
+		scores_sort = sorted(scores, key = lambda s: s[1]) #sort by the score
+		sorted_neighbours = [s[0] for s in scores_sort] #put the moves in order into the sorted neighbors list
+		return sorted_neighbours #return the list
 
-		for neighbor in neighbor_list:
-			np_value = self.board[neighbor[0]][neighbor[1]]
-			if np_value == 0:
-				empty_neighbours.append(neighbor)
-
-		scores = []
-		for empty in empty_neighbours:
-			score = [empty, 0]
-			moves = self.generate_legal_moves(empty)
-			for m in moves:
-				if self.board[m[0]][m[1]] == 0:
-					score[1] += 1
-			scores.append(score)
-
-		scores_sort = sorted(scores, key = lambda s: s[1])
-		sorted_neighbours = [s[0] for s in scores_sort]
-		return sorted_neighbours
-	def tour(self, n, path, to_visit):
-		"""
-		Recursive definition of knights tour. Inputs are as follows:
-		n = current depth of search tree
-		path = current path taken
-		to_visit = node to visit
-		"""
-		self.board[to_visit[0]][to_visit[1]] = n
+	def tour(self, n, path, to_visit): #n = current depth of search tree, path = current path taken, to_visit = node to visit
+		self.board[to_visit[0]][to_visit[1]] = n #put the current count in the new position
 		path.append(to_visit) #append the newest vertex to the current point
-		#print "Visiting: ", to_visit
 		if n == self.w * self.h: #if every grid is filled
-			#self.print_board()
-			#print path
-			#print "Done solving!"
-			prev_x,prev_y = path[0][0],path[0][1]
-			for space in path:
-				sense.set_pixel(prev_x,prev_y,p)
-				sense.set_pixel(space[0],space[1],k)
-				prev_x,prev_y = space[0],space[1]
-				sleep(0.25)
-			sys.exit(1)
-		else:
-			sorted_neighbours = self.sort_lonely_neighbors(to_visit)
-			for neighbor in sorted_neighbours:
-				self.tour(n+1, path, neighbor)
+			prev_x,prev_y = path[0][0],path[0][1] #save the starting position as the new position
+			for space in path: #mark the path
+				sense.set_pixel(prev_x,prev_y,p) #mark the previous position with a lighter color
+				sense.set_pixel(space[0],space[1],k) #mark the current position of the knight
+				prev_x,prev_y = space[0],space[1] #save the current position as the previous position
+				sleep(0.25) #give the user a chance to see where things are (animation speed)
+			sys.exit(1) #exit the script because it was successful
+		else: #not every space is filled
+			sorted_neighbours = self.sort_lonely_neighbors(to_visit) #figure out the best order to try legal moves against
+			for neighbor in sorted_neighbours: #for every legal move
+				self.tour(n+1, path, neighbor) #recursion point
 			#If we exit this loop, all neighbours failed so we reset
-			self.board[to_visit[0]][to_visit[1]] = 0
+			self.board[to_visit[0]][to_visit[1]] = 0 #mark that new spot with a 0 to indicate it can't be done from this path
 			try:
-				path.pop()
-				print "Going back to: ", path[-1]
-			except IndexError:
-				print "No path found"
-				sys.exit(1)
+				path.pop() #take the most recent attempt off the path
+			except IndexError: #if there's nothing on the path to take off...
+				sys.exit(1) #exit unsuccessfully
 
-kt = KnightsTour(8, 8)
-startpos = tuple(int(x.strip()) for x in raw_input('Enter the starting position in the format X,Y: ').split(','))
-#kt.tour(1, [], (0,0))
-kt.tour(1,[],startpos)
+kt = KnightsTour(8, 8) #instantiate the board
+startpos = tuple(int(x.strip()) for x in raw_input('Enter the starting position in the format X,Y: ').split(',')) #prompt for starting position
+#kt.tour(1, [], (0,0)) #start looking for a path from 0,0
+kt.tour(1,[],startpos) #start looking for a path from the prompted location
